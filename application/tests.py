56411d74b8763bc9189ed16a9a2ec88b686798fc
KarenNgala <ngalalalu@gmail.com>
2024-07-20 22:26:14 +0300

    tests , CI

--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Customer, Order
 
-# Create your tests here.
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        Customer.objects.create(name="Jane Doe", code="JD432", phone_number="1234567890")
+
+    def test_customer_creation(self):
+        jane = Customer.objects.get(name="Jane Doe")
+        self.assertEqual(jane.code, "JD432")
+        self.assertEqual(jane.phone_number, "1234567890")
+
+
+class OrderTestCase(TestCase):
+    def setUp(self):
+        customer = Customer.objects.create(name="Jane Doe", code="JD432")
+        Order.objects.create(customer=customer, item="Laptop", amount=35000.00)
+
+    def test_order_creation(self):
+        order = Order.objects.get(item="Laptop")
+        self.assertEqual(order.amount, 35000.00)
+
